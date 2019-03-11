2844a84cd5009cd5af9fdd52912f86d6902320ca
Mahe <mahendrapriyadi@gmail.com>
2019-03-11 08:12:31 +0700

    Create Daftar.php
    membuat controller Daftar.php

---
+++ application/controllers/Daftar.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Daftar extends CI_Controller{
+  //load model
+  public function __construct(){
+    parent::__construct();
+  }
+
+  //Main page Tentang Kita
+  public function index(){
+    $data = array(  'title'   =>    'Pendaftaran');
+    $this->load->view('daftar/list',$data,FALSE);
+  }
+}
