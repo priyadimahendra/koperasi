3d4665441221f192a26f8017b18bb7b95bbcdf38
Mahe <mahendrapriyadi@gmail.com>
2019-05-21 11:24:14 +0700

    Update User_model.php
    menambahkan fungsi detail

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -9,13 +9,23 @@ class User_model extends CI_Model
     $this->load->database();
   }
 
+  public function detail($id_user)
+  {
+    $this->db->select('*');
+    $this->db->from('user');
+    $this->db->where('id_user',$id_user);
+    $this->db->order_by('id_user','DESC');
+    $query  = $this->db->get();
+    return $query->row();
+  }
+
   //Login
   public function login($username , $password){
     $this->db->select('*');
     $this->db->from('user');
     $this->db->where(array( 'username'   =>    $username,
                             'password'   =>    sha1($password)));
-    $this->db->order_by('id_user');
+    $this->db->order_by('id_user','DESC');
     $query = $this->db->get();
     return $query->row();
   }
