35ddb7ca950b1502c9394b913c67eac261895add
Mahe <mahendrapriyadi@gmail.com>
2019-05-21 11:25:56 +0700

    Update wrapper.php
    memberri batasan agar admin masuk dahulu

--- application/views/admin/layout/wrapper.php
+++ application/views/admin/layout/wrapper.php
@@ -1,9 +1,9 @@
 <?php
 
-//if($this->session->userdata('username') == ""){
-//  $this->session->set_flashdata('sukses', 'Silahkan login dahulu');
-//  redirect(base_url('login'), 'refresh');
-//}
+if($this->session->userdata('username') == ""){
+  $this->session->set_flashdata('sukses', 'Silahkan login dahulu');
+  redirect(base_url('login'), 'refresh');
+}
 
 require_once('head.php');
 require_once('header.php');
