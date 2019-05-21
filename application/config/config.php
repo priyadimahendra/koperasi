487093705b6560991783d30fc489f94226a7b137
Mahe <mahendrapriyadi@gmail.com>
2019-05-21 11:22:49 +0700

    Update config.php
    mengubah csrf protection menjadi false

--- application/config/config.php
+++ application/config/config.php
@@ -448,7 +448,7 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_regenerate' = Regenerate token on every submission
 | 'csrf_exclude_uris' = Array of URIs which ignore CSRF checks
 */
-$config['csrf_protection'] = TRUE;
+$config['csrf_protection'] = FALSE;
 $config['csrf_token_name'] = 'Koperasi_HalalUmmat';
 $config['csrf_cookie_name'] = 'Koperasi_HalalUmmat';
 $config['csrf_expire'] = 7200;
