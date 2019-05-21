2054d4cd1f027c4a2ec785712ea14a21366985f2
Mahe <mahendrapriyadi@gmail.com>
2019-05-21 11:25:31 +0700

    Update header.php
    membuat  setiap tempat menjadi mengambil data username admin yang login

--- application/views/admin/layout/header.php
+++ application/views/admin/layout/header.php
@@ -1,3 +1,8 @@
+<?php
+$id_user    = $this->session->userdata('id_user');
+$nama    = $this->session->userdata('username');
+ ?>
+
 <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
@@ -12,8 +17,8 @@
 padding: 15px 50px 5px 50px;
 float: right;
 font-size: 16px;"> <?php echo date('d M Y') ?> &nbsp;
-<a class="btn btn-success square-btn-adjust"><i class="fa fa-user"></i>User</a>
-<a href="<?php echo base_url() ?>" class="btn btn-primary square-btn-adjust" target="_blank"><i class="fa fa-home"></i>Homepage</a> 
+<a class="btn btn-success square-btn-adjust"><i class="fa fa-user"></i><?php echo $nama ?></a>
+<a href="<?php echo base_url() ?>" class="btn btn-primary square-btn-adjust" target="_blank"><i class="fa fa-home"></i>Homepage</a>
 <a href="<?php echo base_url('login/logout') ?>" class="btn btn-danger square-btn-adjust"><i class="fa fa-sign-out"></i>Logout</a>
 </div>
         </nav>
